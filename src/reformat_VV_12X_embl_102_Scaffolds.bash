f9e90453208f6e42cdca1a08ba781819b9460551
Timothée Flutre <timothee.flutre@supagro.inra.fr>
2015-03-25 11:45:53 +0100

    update reformat scripts to use VITVI

--- src/reformat_VV_12X_embl_102_Scaffolds.bash
+++ src/reformat_VV_12X_embl_102_Scaffolds.bash
@@ -1,27 +1,35 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2014 Institut National de la Recherche Agronomique (INRA)
+# Copyright (C) 2014-2014 Institut National de la Recherche Agronomique (INRA)
 # License: GPL-3+
-# Author: Timothée Flutre
+# Persons: Timothée Flutre [cre,aut]
 
 # Before: >FN594950 Vitis vinifera, line PN40024, scaffold_0.assembly12x
-# After: >scaffold_0 FN594950|Vitis vinifera|PN40024|assembly12x
+# After: >scaffold_0 FN594950|Vitis vinifera|PN40024|assembly12x.0
 
-outPrefix="Vvin-PN40024-12x-scaff"
+set -e -o pipefail -u # http://stackoverflow.com/a/69808/597069
+
+outPrefix="VITVI_PN40024_12x_v0_scaffolds_EMBL_r102"
 
 if [ ! -f VV_12X_embl_102_Scaffolds.fsa.gz ]; then
   echo -e "ERROR: can't find file VV_12X_embl_102_Scaffolds.fsa.gz\n" 1>&2
   exit 1
 fi
 
+date
+echo "start ..."
+
 zcat VV_12X_embl_102_Scaffolds.fsa.gz \
   | awk 'BEGIN{RS=">"} {if(NF==0)next; \
 split($0,a,"\n"); split(a[1],b,", "); split(b[3],c,"."); \
-split(b[1],d," "); split(b[2],e," "); \
-printf ">"c[1]" "d[1]"|"d[2]" "d[3]"|"e[2]"|"c[2]"\n";
+gsub(" ","",c[2]); split(b[1],d," "); split(b[2],e," "); \
+printf ">"c[1]" "d[1]"|"d[2]" "d[3]"|"e[2]"|"c[2]".0\n";
 for(i=2;i<length(a);++i)printf a[i]"\n"; \
 printf a[length(a)]}' \
   > ${outPrefix}.fa
 
 rm -f ${outPrefix}.fa.gz
 gzip ${outPrefix}.fa
+
+echo "done!"
+date
--- src/reformat_VV_chr12x.bash
+++ src/reformat_VV_chr12x.bash
@@ -1,27 +1,35 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2014 Institut National de la Recherche Agronomique (INRA)
+# Copyright (C) 2014-2015 Institut National de la Recherche Agronomique (INRA)
 # License: GPL-3+
-# Author: Timothée Flutre
+# Persons: Timothée Flutre [cre,aut]
 
 # Before: >FN597015 Vitis vinifera, line PN40024, unoriented chromosome_1, chr1
-# After: >chr1 FN597015|Vitis vinifera|PN40024|assembly12x|unoriented chromosome_1
+# After: >chr1 FN597015|Vitis vinifera|PN40024|assembly12x.0|unoriented chromosome_1
 
-outPrefix="Vvin-PN40024-12x-chr"
+set -e -o pipefail -u # http://stackoverflow.com/a/69808/597069
+
+outPrefix="VITVI_PN40024_12x_v0_chroms_URGI"
 
 if [ ! -f VV_chr12x.fsa.gz ]; then
   echo -e "ERROR: can't find file VV_chr12x.fsa.gz\n" 1>&2
   exit 1
 fi
 
+date
+echo "start ..."
+
 zcat VV_chr12x.fsa.gz \
   | awk 'BEGIN{RS=">"} {if(NF==0)next; \
 split($0,a,"\n"); split(a[1],b,","); gsub(" ","",b[4]); \
 sub(" ","|",b[1]); split(b[2],c," "); sub(" ","",b[3]); \
-printf ">"b[4]" "b[1]"|"c[2]"|assembly12x|"b[3]"\n";
+printf ">"b[4]" "b[1]"|"c[2]"|assembly12x.0|"b[3]"\n";
 for(i=2;i<length(a);++i)printf a[i]"\n"; \
 printf a[length(a)]}' \
   > ${outPrefix}.fa
 
 rm -f ${outPrefix}.fa.gz
 gzip ${outPrefix}.fa
+
+echo "done!"
+date
