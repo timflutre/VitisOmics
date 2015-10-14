bde2959b01ad0367b4df5dbdef3c15da5e59589b
Timothée Flutre <timothee.flutre@supagro.inra.fr>
2015-10-14 15:13:59 +0200

    confirm 12x scaffolds have same seq from URGI and NCBI

--- src/vitisomics.R
+++ src/vitisomics.R
@@ -37,26 +37,36 @@ agp.gap <- read.table(file=pipe(cmd), header=TRUE, sep="\t")
 str(agp.gap) # 16063 rows
 
 ## ---------------------------------------------------------------------------
-## task: compare URGI and NCBI fasta file
+## task: compare URGI and NCBI fasta files for 12x scaffolds
 
 library(Biostrings) # http://bioconductor.org
 
-VITVI.PN40024.8x.chroms.URGI <-
-  readDNAStringSet(filepath="results/urgi/VITVI_PN40024_8x_chroms_URGI.fa.gz",
+urgi <-
+  readDNAStringSet(filepath="results/urgi/VITVI_PN40024_12x_v0_scaffolds_EMBL_r102.fa.gz",
                    format="fasta")
-length(VITVI.PN40024.8x.chroms.URGI) # 35
-names(VITVI.PN40024.8x.chroms.URGI) # chr1, chr10, chr10_random, etc
-width(VITVI.PN40024.8x.chroms.URGI)
-letterFrequency(VITVI.PN40024.8x.chroms.URGI, letters=c("A","T","G","C","N"))
+length(urgi) # 2059
+names(urgi[1]) # "scaffold_0 FN594950|Vitis vinifera|PN40024|assembly12x.0"
+width(urgi[1]) # 13101952
+letterFrequency(urgi[1], letters=c("A","T","G","C","N"))
 
-VITVI.PN40024.8x.chroms.NCBI <-
-  readDNAStringSet(filepath="results/ncbi/VITVI_PN40024_8x_chroms_NCBI.fa.gz",
+ncbi <-
+  readDNAStringSet(filepath="results/ncbi/VITVI_PN40024_12x_v0_scaffolds_NCBI.fa.gz",
                    format="fasta")
-length(VITVI.PN40024.8x.chroms.NCBI) # 3343
-head(names(VITVI.PN40024.8x.chroms.NCBI), n=20) #
-width(VITVI.PN40024.8x.chroms.NCBI)
-letterFrequency(VITVI.PN40024.8x.chroms.NCBI, letters=c("A","T","G","C","N"))
+length(ncbi) # 2059
+names(ncbi[1]) # "scaffold_11 NW_003723981.1|Vitis vinifera|PN40024|assembly12x.0|localized chr1"
+width(ncbi[1]) # 6551432
+letterFrequency(ncbi[1], letters=c("A","T","G","C","N"))
 
-names(VITVI.PN40024.8x.chroms.URGI[1])
-names(VITVI.PN40024.8x.chroms.NCBI[1])
-VITVI.PN40024.8x.chroms.URGI[1] == VITVI.PN40024.8x.chroms.NCBI[1] # TRUE
+fa.head <- data.frame(urgi=sapply(strsplit(names(urgi), " "), function(x){x[1]}),
+                      ncbi=sapply(strsplit(names(ncbi), " "), function(x){x[1]}),
+                      stringsAsFactors=FALSE)
+
+all(sort(fa.head$urgi) == sort(fa.head$ncbi)) # TRUE
+
+for(i in 1:nrow(fa.head)){
+  if(urgi[which(fa.head$urgi == fa.head$urgi[i])] !=
+     ncbi[which(fa.head$ncbi == fa.head$urgi[i])]){
+    message(fa.head$urgi[i])
+    break
+  }
+} ## all good!
