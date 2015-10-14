8591585982353a3c88d134a9749d65a278d0a7d0
Timothée Flutre <timothee.flutre@supagro.inra.fr>
2015-10-14 19:21:55 +0200

    reformat 8x scaffs from NCBI

--- src/reformat_scaffs_NCBI-8x.bash
+++ src/reformat_scaffs_NCBI-8x.bash
@@ -6,17 +6,17 @@
 
 # http://www.ncbi.nlm.nih.gov/Sitemap/sequenceIDs.html
 
-# for assembled scaffolds:
-# Before: >gi|225570623|ref|NC_012007.2|NC_012007 Vitis vinifera chromosome 1, reference assembly (based on 8x_WGS), whole genome shotgun sequence
-# After: >chr1 NC_012007.2|Vitis vinifera|PN40024|assembly8x|chromosome_1
+# Localized sequence:
+# Before: >gi|221070425|ref|NW_002238163.1|Vvi1_WGA9_1 Vitis vinifera chromosome 1 genomic contig, reference assembly (based on 8x_WGS scaffold_75)
+# After: >scaffold_75 NW_002238163.1|Vitis vinifera|PN40024|assembly8x|chr1
 
-# for unassembled scaffolds:
+# Unplaced sequence:
 # Before: >gi|221071967|ref|NW_002240929.1|VviUn_WGA355_1 Vitis vinifera genomic contig, reference assembly (based on 8x_WGS scaffold_3735)
-# After: >chrUn_scaffold_3735 NW_002240929.1|Vitis vinifera|PN40024|assembly8x|chromosome_Un
+# After: >scaffold_3735 NW_002240929.1|Vitis vinifera|PN40024|assembly8x|unplaced chrUn
 
 set -e -o pipefail -u # http://stackoverflow.com/a/69808/597069
 
-outPrefix="VITVI_PN40024_8x_chroms_NCBI"
+outPrefix="VITVI_PN40024_8x_scaffolds_NCBI"
 
 date
 echo "start ..."
@@ -30,12 +30,14 @@ for chr in {1..19}; do
   fi
   zcat vvi_ref_chr${chr}.fa.gz \
     | awk -v chr=${chr} 'BEGIN{RS=">"} {if(NF==0)next; \
-split($0,a,"\n"); split(a[1],b,"|"); \
-printf ">chr"chr" "b[4]"|Vitis vinifera|PN40024|assembly8x|chromosome_"chr"\n";
+split($0,a,"\n"); split(a[1],b,"|"); split(b[5],c," "); \
+gsub(")","",c[13]); \
+printf ">"c[13]" "b[4]"|Vitis vinifera|PN40024|assembly8x|chr"chr"\n";
 for(i=2;i<length(a);++i)printf a[i]"\n"; \
 printf a[length(a)]}' \
           >> ${outPrefix}.fa
 done
+
 chr="Un"
 if [ ! -f vvi_ref_chr${chr}.fa.gz ]; then
   echo -e "ERROR: can't find file vvi_ref_chr${chr}.fa.gz\n" 1>&2
@@ -43,9 +45,9 @@ if [ ! -f vvi_ref_chr${chr}.fa.gz ]; then
 fi
 zcat vvi_ref_chr${chr}.fa.gz \
   | awk -v chr=${chr} 'BEGIN{RS=">"} {if(NF==0)next; \
-split($0,a,"\n"); split(a[1],b,"|"); \
-split(b[5],c," "); gsub(")","",c[11]); \
-printf ">chr"chr"_"c[11]" "b[4]"|Vitis vinifera|PN40024|assembly8x|chromosome_"chr"\n";
+split($0,a,"\n"); split(a[1],b,"|"); split(b[5],c," "); \
+gsub(")","",c[11]); \
+printf ">"c[11]" "b[4]"|Vitis vinifera|PN40024|assembly8x|chr"chr"\n";
 for(i=2;i<length(a);++i)printf a[i]"\n"; \
 printf a[length(a)]}' \
         >> ${outPrefix}.fa
